8dc25c2ffe11e8bd227d372c108b8402bdbadd42
marsal <marsal@DESKTOP-SL3HUAK>
2021-07-10 15:05:59 +0530

    Add project files.

---
+++ ServicewithInterfaces/Client/Resources/IRepository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ServicewithInterfaces.Client.Resources
+{
+   public interface IRepository
+    {
+        List<Data> GetData();
+    }
+}
---
+++ ServicewithInterfaces/Client/Resources/RepositoryInMemory.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ServicewithInterfaces.Client.Resources
+{
+    public class RepositoryInMemory : IRepository
+    {
+        public List<Data> GetData()
+        {
+            return new List<Data>()
+            {
+
+
+                new Data(){Id=1, Name="Amit", Address="Noida"},
+                new Data(){Id=2, Name="Sumit", Address="Gr Noida"},
+                new Data(){Id=3, Name="Sonu", Address="Delhi"}
+
+            };
+        }
+        
+    }
+}
